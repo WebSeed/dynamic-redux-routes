183dec99953867a4a0ab2280773d62f3dc98ee2c
Will Bamford <w.bamford@gmail.com>
2016-10-26 13:56:27 +0100

    Redux foundation

---
+++ webpack.config.babel.js
@@ -0,0 +1,42 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import webpack from 'webpack'
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import CleanWebpackPlugin from 'clean-webpack-plugin'
+
+export default {
+  entry: {
+    main: path.join(__dirname, 'src'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+    publicPath: '/',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+    }, {
+      test: /\.json$/,
+      loader: 'json',
+    }],
+  },
+  resolve: {
+    modulesDirectories: ['src', 'node_modules'],
+    extensions: ['', '.js', '.jsx', '.scss'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    stats: 'errors-only',
+  },
+  devtool: 'source-map',
+}
